c53720300977ee9928cd16ef0d5849b238a7f860
Faheem <faheemmbasheer@gmail.com>
2024-09-19 18:26:29 +0530

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const letters = string.split("");
+    let reverseString = "";
+    for (let i = letters.length - 1; i >= 0; i--) {
+        reverseString += letters[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
