3e9e66e38be8e2f70828cc05bdcc4a4af5c7d17c
Faheem <faheemmbasheer@gmail.com>
2024-09-21 14:39:10 +0530

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    // validating input
+    if (typeof(x) != "number" || typeof(y) != "number" || x < 0 || y < 0 || Math.floor(x) != x || Math.floor(y) != y) {
+        return "ERROR";
+    }
 
+    // switching variables if x is bigger
+    if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+
+    // calculating sum
+    let sum = 0;
+    for(let i = x; i <= y; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
