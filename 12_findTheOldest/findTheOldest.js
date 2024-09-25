6d673798f6a37fb6ffb1676a2466ef5511406a0b
Faheem <faheemmbasheer@gmail.com>
2024-09-25 10:30:28 +0530

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // year of death - year of bith = age
+    // if year of death not defined, use currentyear
+    const currentYear = new Date().getFullYear();
+    const oldest = people.reduce((oldest, person) => {
+        const oldAge = getAge(
+            oldest.yearOfBirth,
+            oldest.yearOfDeath
+        );
+        const currentAge = getAge(
+            person.yearOfBirth,
+            person.yearOfDeath
+        );
 
+        return currentAge > oldAge ? person : oldest;
+    });
+    return oldest;
 };
 
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
