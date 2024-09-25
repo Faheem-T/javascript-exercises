e8ab16109034819497f7db265ff4be937c6be9ce
Faheem <faheemmbasheer@gmail.com>
2024-09-25 09:38:33 +0530

    complete exercise 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const joinedString = Array.from(string.toLowerCase())
+                    .filter(char => /[a-zA-Z0-9]/.test(char))
+                    .join("");
+    
+    // 
+    const strLen = joinedString.length - 1;
+    for (let i = 0; i < Math.floor(strLen / 2); i++) {
+        if (joinedString[i] !== joinedString[strLen - i]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // Changing to number if string
+    if (typeof(n) == "string") {
+        n = +n;
+    }
 
+    // Handling negative numbers
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    // Setting the initial two elements of the sequence
+    let sequence  = [1, 1];
+
+    for (let i = 2; i < n; i++) {
+        let lastElement = sequence[i - 1];
+        let lastToLastElement = sequence[i - 2];
+        sequence.push(lastElement + lastToLastElement);
+    }
+
+    // Return 0 if n is zero, otherwise return last element of the sequence
+    return (n !== 0 ? sequence.at(n - 1) : 0);
 };
 
 // Do not edit below this line
